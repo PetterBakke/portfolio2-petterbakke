3ffba931d3a14e3786db3914ceb723ea6be1283b
Petter Bjørnstad Bakke <74598862+PetterBakke@users.noreply.github.com>
2022-10-26 11:18:16 +0200

    Add files via upload

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import SEM from "../../Assets/semester.png";
 import JSF from "../../Assets/jsframeworks.png";
 import PRO from "../../Assets/project-exam2.png";
+import CAL from "../../Assets/calculator.png";
+import RWE from "../../Assets/react-weather.png";
 
 function Portfolio() {
   return (
@@ -38,6 +40,26 @@ function Portfolio() {
           <div className="portfolio-item cta">
             <a href="https://github.com/PetterBakke/project-exam-2-petter-bakke" className="btn">Github repo</a>
             <a href="https://project-exam-2-petter-bakke.vercel.app/" className="btn btn-primary">Live site</a>
+          </div>
+        </article>
+        <article className="portfolio-item">
+          <div className="portfolio-item-image">
+            <img src={CAL} alt="calculator project" />
+          </div>
+          <h3>This is my Calculator</h3>
+          <div className="portfolio-item cta">
+            <a href="https://github.com/PetterBakke/calculator-react-petter-bakke" className="btn">Github repo</a>
+            <a href="https://calculator-react-petter-bakke.vercel.app/" className="btn btn-primary">Live site</a>
+          </div>
+        </article>
+        <article className="portfolio-item">
+          <div className="portfolio-item-image">
+            <img src={RWE} alt="react weather project" />
+          </div>
+          <h3>This is my React weather app</h3>
+          <div className="portfolio-item cta">
+            <a href="https://github.com/PetterBakke/react-weather-app" className="btn">Github repo</a>
+            <a href="https://react-weather-app-psi-nine.vercel.app/" className="btn btn-primary">Live site</a>
           </div>
         </article>
       </div>
